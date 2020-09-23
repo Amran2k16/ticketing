318d3ba2dfd73469f32dc31d990db1da29fc7556
amran2k16 <amran4hmed@gmail.com>
2020-09-24 00:02:21 +0100

    Error-handler updated to react differently basedo n error thrown

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator'
-import { ValidationHalt } from 'express-validator/src/base';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+
 
 const router = express.Router();
 
@@ -13,17 +15,13 @@ router.post('/api/users/signup', [
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        throw new Error('Invalid email or password')
+        throw new RequestValidationError(errors.array());
     }
-
-    const { email, password } = req.body;
-
     console.log("Creating a user...")
 
     //imagine database is down
-    throw new Error("Error connecting to database");
+    throw new DatabaseConnectionError();
 
-    res.send({})
 });
 
 export { router as singupRouter };
